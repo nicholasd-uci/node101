c007cb6af7f8558dcf991604a493e68b64db786d
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-14 21:15:24 -0800

    rendered let var to terminal - status 200

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
+let firstName = process.argv[2]
+let lastName = process.argv[3]
+let movie = process.argv[4]
 
-
-console.log(` Hello ${process.argv[2]}! `)
+console.log(` Excellent, so your name is ${firstName} ${lastName} and your favorite movie is ${movie} `)
 
 
 
