8acadafeef8df869a7353a5994fd2bea98314764
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-14 21:18:49 -0800

    argv.js file example

---
+++ argv.js
@@ -0,0 +1,8 @@
+
+// process.argv is a globally scooped API of your current file
+// process.argv is a process that happens when a node file is ran
+
+console.log(process.argv)
+
+// running node
+// node argv.js  *press enter
