5e4a0088e6a98bf95d779f9f2634468bd3268768
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-15 11:03:16 -0800

    included pets.js to practice require and module.exports

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+let pets = require('./pets.js')
 
+let dog = pets[0]
+let cat = pets[1]
+
+console.log(dog, cat)
 
 
 
---
+++ pet.js
@@ -0,0 +1,13 @@
+let dog = {
+    name: 'Beef',
+    age: 2,
+    breed: 'Chihuahua'
+  }
+  
+  let cat = {
+    name: 'Loki',
+    age: 6,
+    breed: 'American Shorthair'
+  }
+  
+  module.exports = [dog, cat]
