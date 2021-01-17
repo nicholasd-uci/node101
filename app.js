8357f96c2ecf0551028462b3ac08aa82ddc1b4cc
Nicholas Dallas <nicholas.dallas0@gmail.com>
2021-01-17 10:55:37 -0800

    tested module exporting and importing - status 200

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
+// require statement in order to bring in pets objects.
 let pets = require('./pets.js')
-
+// set variables for each object and grab them by their index.
 let dog = pets[0]
 let cat = pets[1]
-
+// this will rendering the objects info in the terminal.
 console.log(dog, cat)
-
-
-
 
 
 
--- pets.js
+++ pets.js
@@ -1,13 +1,17 @@
+// exporting this file out with two objects.
+
+// Object One
 let dog = {
-    name: 'Beef',
-    age: 2,
-    breed: 'Chihuahua'
-  }
-  
-  let cat = {
-    name: 'Loki',
-    age: 6,
-    breed: 'American Shorthair'
-  }
-  
-  module.exports = [dog, cat]
+  name: 'Beef',
+  age: 2,
+  breed: 'Chihuahua'
+}
+// Object Two
+let cat = {
+  name: 'Loki',
+  age: 6,
+  breed: 'American Shorthair'
+}
+
+// exports file
+module.exports = [dog, cat]
